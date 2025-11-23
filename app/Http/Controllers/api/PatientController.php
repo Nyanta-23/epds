737789ba3a3d3628d76e5c36ea44ee0e442d6754
Jiaddzikri <jiaddzikri140604@gmail.com>
2025-11-23 22:53:57 +0700

    feat(update-patient): add update patient api

---
+++ app/Http/Controllers/api/PatientController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\DTO\Request\Patient\PatientUpdateAttributeRequest;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Patient\PatientUpdateRequestValidator;
+use App\Service\Patient\PatientService;
+use Exception;
+use Log;
+
+class PatientController extends Controller
+{
+  public function __construct(private PatientService $patientService)
+  {
+
+  }
+
+  public function update(PatientUpdateRequestValidator $request, string $id)
+  {
+    try {
+      $validated = $request->validated();
+
+      $request = new PatientUpdateAttributeRequest();
+      $request->name = $validated['name'];
+      $request->phone_number = $validated['phone_number'];
+      $request->birthplace = $validated['birthplace'];
+      $request->date_of_birth = $validated['date_of_birth'];
+      $request->job = $validated['job'];
+      $request->married_status = $validated['married_status'];
+      $request->highest_education = $validated['highest_education'];
+      $request->province = $validated['province'];
+      $request->city_or_district = $validated['city_or_district'];
+      $request->subdistrict = $validated['subdistrict'];
+      $request->village = $validated['village'];
+      $request->province_id = $validated['province_id'];
+      $request->city_or_district_id = $validated['city_or_district_id'];
+      $request->subdistrict_id = $validated['subdistrict_id'];
+      $request->village_id = $validated['village_id'];
+      $request->address = $validated['address'];
+
+      $this->patientService->update($request, $id);
+
+      return response()->json([
+        'message' => 'update successfully'
+      ], 200);
+
+    } catch(Exception $error) {
+      Log::error('update_patient_error', ['error' => $error->getMessage()]);
+      return response()->json([
+        'message' => $error->getMessage()
+      ], $error->getCode());
+    }
+  }
+}
