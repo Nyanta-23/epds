193d4ee0aadb72d14f83e4cd83b20c3e1ac15782
Jiaddzikri <jiaddzikri140604@gmail.com>
2025-11-24 17:33:17 +0700

    feat(baby): add store baby api

---
+++ app/Http/Controllers/api/BabyController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\DTO\Request\Baby\BabyStoreAttributeRequest;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Baby\BabyStoreRequestValidator;
+use App\Service\Baby\BabyService;
+use Exception;
+use Illuminate\Http\Request;
+
+class BabyController extends Controller
+{
+    public function __construct(private BabyService $babyService)
+    {
+
+    }
+
+    public function store(BabyStoreRequestValidator $request) 
+    {
+        try {
+            $request->validated();
+
+            $babyReq = new BabyStoreAttributeRequest();
+
+            $babyReq->which_child = (int) $request->post('which_child');
+            $babyReq->date_of_birth = $request->post('date_of_birth');
+            $babyReq->baby_condition = (int) $request->post('baby_condition');
+            $babyReq->typeof_delivery = (int) $request->post('typeof_delivery');
+            $babyReq->gender = $request->post('gender');
+            $babyReq->mother_id = $request->post('mother_id');
+
+            $response = $this->babyService->store($babyReq);
+
+            return response()->json([
+                'message' => 'successfully add baby',
+                'data' => $response
+            ], 201);
+        } catch (Exception $error) {
+            return response()->json([
+                'message' => $error->getMessage(),
+            ],$error->getCode());
+        }
+    }
+}
