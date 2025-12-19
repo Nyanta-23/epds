cb5ba062704450e4147c55e5571bbfe2c48018f7
Jiaddzikri <jiaddzikri140604@gmail.com>
2025-12-19 08:22:13 +0700

    chore: add new build

---
+++ public/build/assets/married_status-II9itfSO.js
@@ -0,0 +1,1 @@
+const r=e=>{switch(e){case"not_married":return"Belum Menikah";case"married":return"Menikah";case"divorced":return"Bercerai";default:return"-"}};export{r as m};
---
+++ public/build/assets/role-identifier-DNSL2CB1.js
@@ -0,0 +1,1 @@
+const r=e=>{switch(e){case"Patient":return"Pasien";case"Midwife":return"Bidan";case"Admin":return"Admin";case"Super Admin":return"Super Admin";default:return"-"}};export{r};
