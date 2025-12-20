2ea8267cde74cfe3d5d9854f257f695051b97550
Jiaddzikri <jiaddzikri140604@gmail.com>
2025-12-21 02:36:26 +0700

    fix: error handling on properties start_date and end_date

--- resources/js/components/search.tsx
+++ resources/js/components/search.tsx
@@ -15,7 +15,7 @@ export default function Search({ total, filter, link }: SearchProps) {
   const { only_trash, filter_list } = filter;
   const { select_filter, date_filter, search } = filter_list ?? {};
   const { role } = select_filter ?? {};
-  const {start_date, end_date} = date_filter;
+  const {start_date, end_date} = date_filter ?? {};
 
   const [querySearch, setQuerySearch] = useState<string>(search ?? "");
   const [isSearching, setIsSearching] = useState<boolean>(false);
