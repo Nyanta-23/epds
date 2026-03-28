731fffcef9c938bc44f7dd32761da264c943e3ce
Jiaddzikri <jiaddzikri140604@gmail.com>
2026-03-28 15:38:14 +0700

    fix: missing api

--- app/Http/Controllers/Api/PostpartumController.php
+++ app/Http/Controllers/Api/PostpartumController.php
@@ -4,12 +4,15 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Service\PostpartumVisit\PostpartumScheduleService;
+use App\Service\PostpartumVisit\PostpartumVisitService;
 use Illuminate\Http\Request;
 
 class PostpartumController extends Controller
 {
-    public function __construct(private PostpartumScheduleService $postpartumScheduleService)
-    {
+    public function __construct(
+        private PostpartumScheduleService $postpartumScheduleService,
+        private PostpartumVisitService $postpartumVisitService
+    ) {
     }
 
     public function getSchedule(Request $request, string $motherId)
@@ -20,5 +23,29 @@ class PostpartumController extends Controller
             'message' => 'Postpartum schedule retrieved successfully',
             'data' => $schedule
         ]);
+    }
+
+    public function previousPostpartumVisit()
+    {
+        try {
+            $user = auth()->user();
+            $hasPrevious = $this->postpartumVisitService->hasPrevious($user);
+
+            if ($hasPrevious) {
+                $previousPostpartumVisit = $this->postpartumVisitService->previousDataFromUser($user);
+            } else {
+                $previousPostpartumVisit = null;
+            }
+
+            return response()->json([
+                'message' => 'Previous postpartum visit fetched successfully.',
+                'data' => $previousPostpartumVisit
+            ], 200);
+        } catch (\Exception $err) {
+            return response()->json([
+                'message' => 'An error occurred.',
+                'error' => $err->getMessage()
+            ], 500);
+        }
     }
 }
